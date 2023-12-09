a1796ac7561c317df2170e02be00bcb8aeae5c5a
samayyy <samaykgada09@gmail.com>
2023-12-10 01:13:42 +0530

    changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 import { LightNodeProvider } from "@waku/react";
 const NODE_OPTIONS = { defaultBootstrap: true };
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
